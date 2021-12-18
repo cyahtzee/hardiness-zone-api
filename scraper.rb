93fa6cb69a455dddc0b3ae205625ced00e7a1245
matias acuna <agroang@gmail.com>
2021-12-18 11:33:54 +0900

    added some comments at the end of scraper.rb if you are unsure of the amount of results

--- scraper.rb
+++ scraper.rb
@@ -6,3 +6,6 @@ doc = Nokogiri::HTML(html_content)
 doc.search('#post-body-1523308068074273552').each do |element|
   puts element.text.scan(/[0-9]{3}-[0-9]{4}/)
 end
+
+# add .count at the end to make sure you are getting all the results,
+# would have to check by hand using the website...
