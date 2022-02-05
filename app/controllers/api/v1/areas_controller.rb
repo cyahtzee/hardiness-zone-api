cc45ec32f68fa7b1b585d38cc26ee6bfffa0c725
matias acuna <agroang@gmail.com>
2022-02-05 11:12:19 +0900

    added a few more comments about the new approach

--- app/controllers/api/v1/areas_controller.rb
+++ app/controllers/api/v1/areas_controller.rb
@@ -8,7 +8,8 @@ class Api::V1::AreasController < Api::V1::BaseController
     @area = Area.find_by post_code: @post_code
     # need a route if it doesn't find the post_code, maybe target the
     # first 3 numbers and return all the available? for Tokyo it's very
-    # broad
+    # broad, some of them have 4 numbers so get that first, otherwise the
+    # first 3
     authorize @area
   end
 end
